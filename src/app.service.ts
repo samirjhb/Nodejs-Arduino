3598f95e4f7e4753604dd03d3d6bf5d52c2b734a
root <root@localhost.localdomain>
2022-11-24 16:56:59 -0300

    avance

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,38 +1,39 @@
 import { Injectable } from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ReadlineParser, SerialPort } from 'serialport';
 
 @Injectable()
 export class AppService {
-   getHello() {
-    try {
-      const port = new SerialPort({
-        path: "COM3",
-        baudRate: 9600,
-      });
+  getHello() {
+    return `This action returns all conexionArduino`;
+    //   const port = new SerialPort(
+    //     {
+    //       path: '/dev/ttyACM0',
+    //       baudRate: 9600,
+    //     },
+    //     function (err) {
+    //       if (err) {
+    //         return console.log('Error: ', err.message);
+    //       }
+    //     },
+    //   );
+    //   const parser = new ReadlineParser();
+    //   port.pipe(parser);
+    //   parser.on('data', (data) => {
+    //     const user = parseInt(data);
+    //     const number = 147147224011;
 
-      const parser = new ReadlineParser()
-      port.pipe(parser)
+    //     if (user == number) {
+    //       console.log(' user registrado');
+    //     } else {
+    //       console.log(' user no registrado');
+    //     }
 
-     parser.on('data', (data) => {
-        const user = parseInt(data);
-        const number = 147147224011;
-
-        if (user ==number ) {
-          console.log(' user registrado');
-          
-        } else {
-          console.log(' user no registrado');
-        }
-
-        console.log(user);
-        
-
-      })
-
-  
-    } catch (error) {
-      console.log(error);
-    }
-
+    //     console.log(user);
+    //   });
+    //   // Open errors will be emitted as an error event
+    //   parser.on('error', function (err) {
+    //     console.log('Error: ', err.message);
+    //   });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
+import { ReadlineParser, SerialPort } from 'serialport';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const port = new SerialPort(
+    {
+      path: '/dev/ttyACM0',
+      baudRate: 9600,
+    },
+    function (err) {
+      if (err) {
+        return console.log('Error: ', err.message);
+      }
+    },
+  );
+
+  const parser = new ReadlineParser();
+  port.pipe(parser);
+
+  parser.on('data', (data) => {
+    const user = parseInt(data);
+    const number = 147147224011;
+
+    if (user == number) {
+      console.log(' user registrado');
+    } else {
+      console.log(' user no registrado');
+    }
+
+    console.log(user);
+  });
+
+  // Open errors will be emitted as an error event
+  parser.on('error', function (err) {
+    console.log('Error: ', err.message);
+  });
+
   await app.listen(3000);
 }
 bootstrap();
