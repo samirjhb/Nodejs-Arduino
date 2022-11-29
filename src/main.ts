b88c8796e14c11fd2138cf3c7dcaddb21e3cfe1b
samirjhb <samirmac98@gmail.com>
2022-11-29 18:18:52 -0300

    Modificacion main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
@@ -58,13 +59,32 @@ async function bootstrap() {
       .then(function (response) {
         if (response.data.userID == user) {
           console.log(' user registrado');
+          // volver nueva fecha y hora
+          const Tiempo = new Date();
+          //Contador con la hora actual
+          function showTime() {
+            // volver nueva fecha y hora
+            const dateTime = new Date();
+            // devolver el tiempo
+            const time = dateTime.toLocaleTimeString();
+          }
+          //ejecutar la funcion de contador
+          setInterval(showTime, 1000);
+          setTimeout(() => {
+            transporter.sendMail({
+              from: '"Conductor Fatigado 😴 🚛" <prueba@example.com>',
+              to: 'samirmac98@gmail.com, h.mendez@grupofirma.cl, g.aravena@grupofirma.cl',
+              subject: `Conductor "${response.data.userNombre}" esta Fatigado 😴 🚛`,
+              html: `<b> Conductor con el ID "${user}"  presenta Fatigacion por exceso de hora de conducion 🚛🚛🚛 </b>`, // html body
+            });
+          }, 180000);
+
           transporter.sendMail({
             from: '"PROBANDO CV" <prueba@example.com>',
-            to: 'samirmac98@gmail.com, h.mendez@grupofirma.cl',
+            to: 'samirmac98@gmail.com, h.mendez@grupofirma.cl, dios@grupofirma.cl',
             subject: 'User Arranco ✔',
-            html: `<b>User Registrado con el ID Arranco Camion "${user}"  y el nombre "${response.data.userNombre}"</b>`, // html body
+            html: `<b>User Registrado con el ID Arranco Camion "${user}"  y el nombre "${response.data.userNombre}" a la hora "${Tiempo}"  </b>`, // html body
           });
-          port.write('9');
         } else {
           console.log(' user no registrado');
           transporter.sendMail({
